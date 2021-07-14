f82a6c476867210d1543f2c058800693eed8a3d1
Someshbose <iamsomeshbose@gmail.com>
2021-07-14 22:41:22 +0530

    fixing the bug

--- src/test/java/someshbose/github/io/springsecurity/custom/MainTest.java
+++ src/test/java/someshbose/github/io/springsecurity/custom/MainTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,7 +17,7 @@ public class MainTest {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(username = "Somesh")
+    @WithCustomUser(username = "Somesh")
     public void helloAuthenticated() throws Exception{
         mockMvc.perform(get("/hello")).andExpect(status().isOk());
     }
--- src/test/java/someshbose/github/io/springsecurity/custom/WithCustomUser.java
+++ src/test/java/someshbose/github/io/springsecurity/custom/WithCustomUser.java
@@ -1,10 +1,12 @@
 package someshbose.github.io.springsecurity.custom;
 
+import org.springframework.security.test.context.support.WithSecurityContext;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-
+@WithSecurityContext(factory = CustomSecurityContextFactory.class)
 public @interface WithCustomUser {
 
     String username();
